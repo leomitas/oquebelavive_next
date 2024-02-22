2fc2c5e4715411bfebae6f9bb5c380ce36c530e3
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2024-02-22 20:08:30 -0300

    fix: responsive navbar for small px

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,18 +5,24 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <nav className='bg-lavender-rose-1 fixed top-0 left-0 z-50 w-screen'>
-      <ul className='max-w-7xl mx-auto px-8 xl:px-0 flex justify-between p-2'>
+      <ul className='max-w-7xl mx-auto px-4 lg:px-8 xl:px-0 flex justify-between p-2'>
         <li className='flex items-center'>
           <h2 className='text-black font-bold text-xl'>Oquebelavive</h2>
         </li>
-        <li>
+        <li className='flex gap-4'>
           <Link href={'/'}>
-            <Button variant={'link'} className='font-bold text-black text-xl'>
+            <Button
+              variant={'link'}
+              className='font-bold text-black text-xl p-0'
+            >
               Home
             </Button>
           </Link>
           <Link href={'#contacts'}>
-            <Button variant={'link'} className='font-bold text-black text-xl'>
+            <Button
+              variant={'link'}
+              className='font-bold text-black text-xl p-0'
+            >
               Contatos
             </Button>
           </Link>
