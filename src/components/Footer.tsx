8967dc55f53de1deb8b8b80454ca483f3ca954ea
Leonardo Abreu <leonardo.abreu.dev@gmail.com>
2024-07-09 17:50:02 -0300

    feat: update footer, add privacy policy and terms and conditions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='flex justify-between max-w-7xl mx-auto p-4 xl:px-0 border-t-2 border-black'>
-      <p>Oquebelavive. © 2024 Todos direitos reservados. </p>
+      <p>© 2024 Oquebelavive. Todos direitos reservados | <a href="https://www.termsfeed.com/live/0fcd2e60-84dc-4d28-a178-86561bc7bc71">Política de Privacidade</a> | <a href="https://www.termsfeed.com/live/9325fecc-01e6-4111-ae32-14f71a6282d7">Termos de Serviço</a></p>
       <div className='flex gap-1'>
         <p>Desenvolvido por:</p>
         <a href='https://portfolio-leomitas.vercel.app/'>Leonardo Abreu</a>
